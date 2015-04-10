4e5cd91ce759a140616c8dc4b361473910d3ac38
Swarna Ananthan <swarna@Swarnas-MBP.home>
2015-04-09 23:23:19 -0400

    Initial Changes

---
+++ CropAlert/app/src/main/java/farming/cropalert/application/CropAlertApplication.java
@@ -0,0 +1,74 @@
+package farming.cropalert.application;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.util.LruCache;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.Volley;
+
+/**
+ * Created by swarna on 4/9/15.
+ */
+public class CropAlertApplication {
+    private static CropAlertApplication instance;
+    private RequestQueue mRequestQueue;
+    private ImageLoader mImageLoader;
+    private Context context;
+
+    public static synchronized CropAlertApplication getInstance(Context context) {
+        if (instance == null) {
+            instance = new CropAlertApplication(context);
+        }
+        return instance;
+    }
+
+
+    private CropAlertApplication(Context context) {
+        this.context = context;
+        mRequestQueue = getRequestQueue();
+
+        mImageLoader = new ImageLoader(mRequestQueue,
+                new ImageLoader.ImageCache() {
+                    private final LruCache<String, Bitmap>
+                            cache = new LruCache<String, Bitmap>(20);
+
+                    @Override
+                    public Bitmap getBitmap(String url) {
+                        return cache.get(url);
+                    }
+
+                    @Override
+                    public void putBitmap(String url, Bitmap bitmap) {
+                        cache.put(url, bitmap);
+                    }
+                });
+
+
+
+    }
+
+    public static CropAlertApplication getInstance() {
+        return instance;
+
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            // getApplicationContext() is key, it keeps you from leaking the
+            // Activity or BroadcastReceiver if someone passes one in.
+            mRequestQueue = Volley.newRequestQueue(context.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+    public ImageLoader getImageLoader() {
+        return mImageLoader;
+    }
+}
